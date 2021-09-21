37145fb4ec57e5bf5997f9b8d4b5d1f0b8f3ff50
VLTSankalpa <wltsankalpa@gmail.com>
2021-09-22 00:27:40 +0530

    build commit-2

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/This React app can be used to visualize the global spread of COVID-19/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/This React app can be used to visualize the global spread of COVID-19/i);
   expect(linkElement).toBeInTheDocument();
 });
